daba710d553afc6228a329693efbf72becdc2496
AeShevch <shevch.ae@gmail.com>
2020-10-03 17:00:22 +0300

    Учебный проект: начинаем программировать

---
+++ src/components/App/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WelComePage from "../welcome-page/welcome-page";
+
+const App = () => {
+  return (
+    <WelComePage
+      errorsCount={4}
+    />
+  );
+};
+
+export default App;
+
