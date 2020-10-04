0493492c492d041399b0c5897a00ecc188e7543d
AeShevch <shevch.ae@gmail.com>
2020-10-04 13:35:59 +0300

    Исправляет ошибку в нейминге WelComePage => WelcomePage

--- src/components/welcome-page/welcome-page.jsx
+++ src/components/welcome-page/welcome-page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const WelComePage = (props) => {
+const WelcomePage = (props) => {
   const {errorsCount} = props;
 
   return (
@@ -23,8 +23,8 @@ const WelComePage = (props) => {
   );
 };
 
-WelComePage.propTypes = {
+WelcomePage.propTypes = {
   errorsCount: PropTypes.number.isRequired,
 };
 
-export default WelComePage;
+export default WelcomePage;
